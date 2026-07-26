dc6f2bd99fa4b385c550caba1f7d843b85c8ec31
Suresh Selvam <settlesmartglobal@gmail.com>
2026-07-26 19:03:36 +0530

    feat: wave-0 foundation

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -1,4 +1,6 @@
 export const env = {
   DATABASE_URL: process.env.DATABASE_URL!,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
   NODE_ENV: process.env.NODE_ENV ?? "development",
 };
